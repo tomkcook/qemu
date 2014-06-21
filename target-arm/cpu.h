3eaca188a44765561e345d164af3f23ac0ac9f1f
Paolo Bonzini <pbonzini@redhat.com>
2014-06-21 14:58:16 +0200

    linux-user: arm: handle CPSR.E correctly in strex emulation
    Now that CPSR.E is set correctly, prepare for when setend will be able
    to change it; bswap data in and out of strex manually by comparing
    SCTLR.B, CPSR.E and TARGET_WORDS_BIGENDIAN (we do not have the luxury
    of using TCGMemOps).
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>

--- target-arm/cpu.h
+++ target-arm/cpu.h
@@ -1492,6 +1492,34 @@ static inline bool bswap_code(bool sctlr_b)
 #endif
 }
 
+#ifdef CONFIG_USER_ONLY
+/* get_user and put_user respectivaly return and expect data according
+ * to TARGET_WORDS_BIGENDIAN, but ldrex/strex emulation needs to take
+ * into account CPSR.E.  Similar to bwap_code, a XOR gives exactly the
+ * required result, we just throw CPSR.E into the mix too:
+ *
+ *            TARGET_WORDS_BIGENDIAN  SCTLR.B  CPSR.E    need swap?
+ *   LE/LE                 no             0      0          no
+ *   LE/BE                 no             0      1          yes
+ *   BE8/LE                yes            0      0          yes
+ *   BE8/BE                yes            0      1          no
+ *   BE32/BE               yes            1      0          no
+ *  (BE32/LE)              yes            1      1          yes
+ *
+ * Officially, BE32 with CPSR.E=1 has "unpredictable" results.  We
+ * implement it as big-endian code, little-endian data.
+ */
+static inline bool arm_cpu_bswap_data(CPUARMState *env)
+{
+    return
+#ifdef TARGET_WORDS_BIGENDIAN
+       1 ^
+#endif
+       arm_sctlr_b(env) ^
+       !!(env->uncached_cpsr & CPSR_E);
+}
+#endif
+
 static inline void cpu_get_tb_cpu_state(CPUARMState *env, target_ulong *pc,
                                         target_ulong *cs_base, int *flags)
 {
