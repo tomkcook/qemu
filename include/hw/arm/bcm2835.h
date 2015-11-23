bf166cebf95dd9c02d95752b2e12013c6c37e7ce
Andrew Baumann <Andrew.Baumann@microsoft.com>
2015-11-23 15:18:03 -0800

    update copyright headers on new files

--- include/hw/arm/bcm2835.h
+++ include/hw/arm/bcm2835.h
@@ -1,3 +1,13 @@
+/*
+ * Raspberry Pi emulation (c) 2012 Gregory Estrade
+ * Upstreaming code cleanup [including bcm2835_*] (c) 2013 Jan Petrous
+ *
+ * Rasperry Pi 2 emulation and refactoring Copyright (c) 2015, Microsoft
+ * Written by Andrew Baumann
+ *
+ * This code is licensed under the GNU GPLv2 and later.
+ */
+
 #ifndef BCM2835_H
 #define BCM2835_H
 
--- include/hw/arm/bcm2835_peripherals.h
+++ include/hw/arm/bcm2835_peripherals.h
@@ -1,8 +1,18 @@
-#include "qemu-common.h"
-#include "hw/sysbus.h"
+/*
+ * Raspberry Pi emulation (c) 2012 Gregory Estrade
+ * Upstreaming code cleanup [including bcm2835_*] (c) 2013 Jan Petrous
+ *
+ * Rasperry Pi 2 emulation and refactoring Copyright (c) 2015, Microsoft
+ * Written by Andrew Baumann
+ *
+ * This code is licensed under the GNU GPLv2 and later.
+ */
 
 #ifndef BCM2835_PERIPHERALS_H
 #define BCM2835_PERIPHERALS_H
+
+#include "qemu-common.h"
+#include "hw/sysbus.h"
 
 #define TYPE_BCM2835_PERIPHERALS "bcm2835_peripherals"
 #define BCM2835_PERIPHERALS(obj) \
