ebc61434c5aa7042fda32a466ca08937129bd76b
Andrew Baumann <Andrew.Baumann@microsoft.com>
2015-11-25 15:54:23 -0800

    tweak copyright headers (these aren't mine... I just moved the code around)

--- include/hw/intc/bcm2835_ic.h
+++ include/hw/intc/bcm2835_ic.h
@@ -1,10 +1,5 @@
 /*
  * Raspberry Pi emulation (c) 2012 Gregory Estrade
- * Upstreaming code cleanup [including bcm2835_*] (c) 2013 Jan Petrous
- *
- * Rasperry Pi 2 emulation and refactoring Copyright (c) 2015, Microsoft
- * Written by Andrew Baumann
- *
  * This code is licensed under the GNU GPLv2 and later.
  */
 
